d92145e48a3824c84bbfe71db639c629a1ec1ac2
nbcyber <nbcyber>
2024-04-26 17:34:10 +0000

    update

--- src/sketches/home.js
+++ src/sketches/home.js
@@ -1,32 +1,58 @@
+
+
 export function sketch(p5) {
     let rotation = 0;
+    let static_gif;
+
+    p5.preload = () => {
+        static_gif = p5.createImg("https://upload.wikimedia.org/wikipedia/commons/0/02/Television_static.gif?20220213052814")
+    }
   
     p5.setup = () => p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
+    let halfWidth = p5.windowWidth / 2
+    let halfHeight = p5.windowHeight / 2
   
     p5.updateWithProps = props => {
       if (props.rotation) {
         rotation = (props.rotation * Math.PI) / 180;
       }
     };
+
+    let fourSquare = () => {
+        p5.fill(50);
+        p5.square(0,0,25);
+        p5.fill(100);
+        p5.square(0,10,25);
+        p5.fill(150);
+        // p5.square(0,20,25);
+        p5.fill(200);
+        p5.square(0,30,25);
+    }
   
     p5.draw = () => {
-      p5.background(100);
+      p5.background(0);
       p5.normalMaterial();
       p5.noStroke();
+
+      static_gif.position(p5.windowWidth/2, p5.windowHeight/2)
+      static_gif.size(0, 0)
   
       p5.push();
-      p5.translate(-35, 0);
-      p5.rotateY(rotation);
-      p5.rotateX(-0.9);
-      p5.box(100);
+      fourSquare()
+
+      p5.fill(255);
+    //   p5.square(0,0,100);
       p5.pop();
-  
-      p5.noFill();
-      p5.stroke(255);
-      p5.push();
-      p5.translate(400, p5.height * 0.35, -200);
-      p5.sphere(300);
-      p5.pop();
+
+
+      
+      // sphere
+    //   p5.noFill();
+    //   p5.stroke(255);
+    //   p5.push();
+    //   p5.translate(400, p5.height * 0.35, -200);
+    //   p5.sphere(300);
+    //   p5.pop();
     };
 
 
